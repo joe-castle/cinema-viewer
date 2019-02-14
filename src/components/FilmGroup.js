a1d7dd7eaa53b38c15683e0cba01bab7fd86aec0
Joe Smith <joesmith0488@gmail.com>
2019-02-14 15:18:06 +0000

    Added collapsible filmgroups

--- src/components/FilmGroup.js
+++ src/components/FilmGroup.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { CardImg, CardBody, CardTitle, CardSubtitle, Row, Col, Badge } from 'reactstrap'
+import { CardImg, CardBody, CardTitle, CardSubtitle, Row, Col, Badge, Collapse } from 'reactstrap'
 
-import { CardCustom, LinkCustom, BadgeWrapper } from './styled/FilmGroup'
+import { Title, CardCustom, LinkCustom, BadgeWrapper } from './styled/FilmGroup'
 
 function renderNewBadge (film) {
   if (!film.userData || (film.userData && film.userData.new !== false)) {
@@ -12,32 +12,49 @@ function renderNewBadge (film) {
   return null
 }
 
-function FilmGroup ({ films, title, user }) {
-  return <>
-    <h2>{title}</h2>
-    <Row>
-      {films.map((film) =>
-        <Col className='mb-4' lg={2} md={3} sm={4} xs={6} key={film._id}>
-          <LinkCustom to={`/films/${film._id}`}>
-            <CardCustom>
-              <CardImg top src={film.poster} />
-              <CardBody className='d-flex flex-column'>
-                <CardTitle className='font-weight-bold' tag='h5'>{film.title}</CardTitle>
-                <CardSubtitle className='mb-2' tag='small'>
-                  {new Date(film.releaseDate).toDateString()}
-                </CardSubtitle>
-                {user && <BadgeWrapper>
-                  {renderNewBadge(film)}
-                  {film.userData && film.userData.watched && <Badge color='warning'>watched</Badge>}
-                  {film.userData && film.userData.unlimited && <Badge color='danger'>unlimited</Badge>}
-                </BadgeWrapper>}
-              </CardBody>
-            </CardCustom>
-          </LinkCustom>
-        </Col>
-      )}
-    </Row>
-  </>
+class FilmGroup extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      collapse: props.collapse
+    }
+  }
+
+  render () {
+    const { films, title, user } = this.props
+
+    return <>
+      {title && <Title>
+        <h2 onClick={() => { this.setState((state) => ({ collapse: !state.collapse })) }}>{title}</h2>
+        <span className={`oi oi-caret-${this.state.collapse ? 'bottom' : 'top'}`} />
+      </Title>}
+      <Collapse isOpen={!this.state.collapse}>
+        <Row>
+          {films.map((film) =>
+            <Col className='mb-4' lg={2} md={3} sm={4} xs={6} key={film._id}>
+              <LinkCustom to={`/films/${film._id}`}>
+                <CardCustom>
+                  <CardImg top src={film.poster} />
+                  <CardBody className='d-flex flex-column'>
+                    <CardTitle className='font-weight-bold' tag='h5'>{film.title}</CardTitle>
+                    <CardSubtitle className='mb-2' tag='small'>
+                      {new Date(film.releaseDate).toDateString()}
+                    </CardSubtitle>
+                    {user && <BadgeWrapper>
+                      {renderNewBadge(film)}
+                      {film.userData && film.userData.watched && <Badge color='warning'>watched</Badge>}
+                      {film.userData && film.userData.unlimited && <Badge color='danger'>unlimited</Badge>}
+                    </BadgeWrapper>}
+                  </CardBody>
+                </CardCustom>
+              </LinkCustom>
+            </Col>
+          )}
+        </Row>
+      </Collapse>
+    </>
+  }
 }
 
 FilmGroup.propTypes = {
--- src/components/styled/FIlmGroup.js
+++ src/components/styled/FIlmGroup.js
@@ -2,6 +2,23 @@ import { Card } from 'reactstrap'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
+export const Title = styled.div`
+  transition: color ease-in .1s;
+  
+  & h2 {
+    display: inline-block;
+  }
+
+  & span {
+    margin-left: .5em;
+  }
+
+  &:hover {
+    cursor: pointer;
+    color: ${({ theme: { primary } }) => `rgb(${primary})`}
+  }
+`
+
 export const CardCustom = styled(Card)`
   box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
   height: 100%;
