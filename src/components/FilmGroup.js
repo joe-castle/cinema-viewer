20d63a597557b050202d52e567d34f06bb96ac54
Joe Smith <joesmith0488@gmail.com>
2019-02-14 10:29:57 +0000

    improved look of film group page

--- src/components/FilmGroup.js
+++ src/components/FilmGroup.js
@@ -1,27 +1,41 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import { Card, CardImg, CardBody, CardTitle } from 'reactstrap'
+import { CardImg, CardBody, CardTitle, CardSubtitle, Row, Col, Badge } from 'reactstrap'
 
-import { RowCustom, ColCustom } from './styled/FilmGroup'
+import { CardCustom, LinkCustom, BadgeWrapper } from './styled/FilmGroup'
 
-function FilmGroup ({ films, title }) {
+function renderNewBadge (film) {
+  if (!film.userData || (film.userData && film.userData.new !== false)) {
+    return <Badge color='success'>new</Badge>
+  }
+
+  return null
+}
+
+function FilmGroup ({ films, title, user }) {
   return <>
     <h2>{title}</h2>
-    <RowCustom>
+    <Row>
       {films.map((film) =>
-        <ColCustom lg={2} sm={4} xs={6} key={film._id}>
-          <Link to={`/films/${film._id}`}>
-            <Card>
+        <Col className='mb-4' lg={2} md={3} sm={4} xs={6} key={film._id}>
+          <LinkCustom to={`/films/${film._id}`}>
+            <CardCustom>
               <CardImg top src={film.poster} />
-              <CardBody>
-                <CardTitle>{film.title}</CardTitle>
+              <CardBody className='d-flex flex-column'>
+                <CardTitle className='font-weight-bold' tag='h5'>{film.title}</CardTitle>
+                <CardSubtitle className='mb-2' tag='small'>
+                  {new Date(film.releaseDate).toDateString()}
+                </CardSubtitle>
+                {user && <BadgeWrapper>
+                  {renderNewBadge(film)}
+                  {film.userData && film.userData.unlimited && <Badge color='danger'>unlimited</Badge>}
+                </BadgeWrapper>}
               </CardBody>
-            </Card>
-          </Link>
-        </ColCustom>
+            </CardCustom>
+          </LinkCustom>
+        </Col>
       )}
-    </RowCustom>
+    </Row>
   </>
 }
 
@@ -32,7 +46,7 @@ FilmGroup.propTypes = {
     dateAdded: PropTypes.string,
     edis: PropTypes.arrayOf(PropTypes.string),
     poster: PropTypes.string,
-    showtimes: PropTypes.arrayOf(PropTypes.string),
+    showtimes: PropTypes.object,
     unlimited: PropTypes.bool,
     url: PropTypes.string,
     user_data: PropTypes.shape({
--- src/components/styled/FIlmGroup.js
+++ src/components/styled/FIlmGroup.js
@@ -1,9 +1,30 @@
-import { Row, Col } from 'reactstrap'
+import { Card } from 'reactstrap'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-export const RowCustom = styled(Row)`
-  margin-bottom: 1em;
+export const CardCustom = styled(Card)`
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  height: 100%;
+  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  }
 `
-export const ColCustom = styled(Col)`
-  margin-bottom: 1em;
+
+export const LinkCustom = styled(Link)`
+  color: rgb(33, 37, 41);
+
+  &:hover {
+    color: rgb(33, 37, 41);
+    text-decoration: none;
+  }
+`
+
+export const BadgeWrapper = styled.div`
+  margin-top: auto;
+
+  & .badge:not(:first-child) {
+    margin-left: .25em;
+  }
 `
