ccf8ac53486dc0ff7a4b1a9bff547fc9a1cc77ff
Joe Smith <joesmith0488@gmail.com>
2019-02-15 09:58:51 +0000

    updated filmUserData model to new DB keys

--- src/data/models/filmUserData.js
+++ src/data/models/filmUserData.js
@@ -6,14 +6,14 @@ const userDataM = multiQ('film_user_data')
 const userDataS = singleQ('film_user_data')
 
 export function getUserData (userId) {
-  return userDataM((col) => col.find({ 'user_id': new ObjectID(userId) }))
+  return userDataM((col) => col.find({ userId: new ObjectID(userId) }))
 }
 
 // FIX: Fix return value, for upserted values no body is returned
-export function insertUpdateUserData (userData) {
+export function insertUpdateUserData (userId, filmId, userData) {
   return userDataS((col) =>
     col.findOneAndUpdate(
-      { 'user_id': userData['user_id'], 'film_id': userData['film_id'] },
-      { $set: { ...userData, 'film_id': new ObjectID(userData['film_id']) } },
+      { userId, filmId: new ObjectID(filmId) },
+      { $set: userData, $setOnInsert: { userId, filmId: new ObjectID(filmId) } },
       { upsert: true }))
 }
