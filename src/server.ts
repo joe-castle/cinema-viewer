4b577b1e0d1e00a597940f6a4188ac400f017241
Joe Smith <joesmith0488@gmail.com>
2019-02-28 14:36:37 +0000

    removed http2 functionality

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,22 @@
-import { createServer, ServerOptions } from 'spdy'
-import { readFileSync } from 'fs'
-import { resolve } from 'path'
-import { Express } from 'express'
-
 import mongo from './data/client'
 import app from './server.app'
 
 const { 
   MONGODB_URI = '', 
-  PORT = 3000,
-  CERT_KEY,
-  CERT_CERT
+  PORT = 3000
 } = process.env
 
 mongo(MONGODB_URI)
-  .then((db) => {
-    const options: ServerOptions = {
-      key: readFileSync(resolve(<string> CERT_KEY)),
-      cert:  readFileSync(resolve(<string> CERT_CERT))
-    }
-    
+  .then((db) => {    
     if (db) {
-      const appWithDb: Express = app(db)
-      const port: string | number = appWithDb.get('port') || PORT
-
-      createServer(options, appWithDb)
-        .listen(port, (err: Error) => {
-          if (err) {
-            console.error(err)
-            process.exit(1)
-          } else {
-            console.log('Express server listening on port:', port)
-          }
-        })
+      app(db).listen(PORT, (err: Error) => {
+        if (err) {
+          console.error(err)
+          process.exit(1)
+        } else {
+          console.log('Express server listening on port:', PORT)
+        }
+      })
     } else {
       console.error('Something went wrong connecting to the DB')
       process.exit(1)
