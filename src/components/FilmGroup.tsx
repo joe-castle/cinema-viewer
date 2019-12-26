67b0108a408bcef93b481459563513cee9c546a3
Joe Smith <joesmith0488@gmail.com>
2019-12-26 17:07:23 +0000

    feat(filmgroup): added tabular view for wathced films

--- src/components/FilmGroup.tsx
+++ src/components/FilmGroup.tsx
@@ -1,20 +1,71 @@
 import React, { useState } from 'react'
-import { CardImg, CardBody, CardTitle, CardSubtitle, Row, Col, Collapse } from 'reactstrap'
+import { CardImg, CardBody, CardTitle, CardSubtitle, Row, Col, Collapse, Table } from 'reactstrap'
 import FuzzySearch from 'fuzzy-search'
 
 import { Title, CardCustom, LinkCustom } from './styled/FilmGroup'
 import BadgeWrapper from './BadgeWrapper'
 import { RouteProps } from 'react-router';
-import { IFilm } from '../types/data';
+import { IFilm, IUser } from '../types/data';
 import { useSelector } from 'react-redux';
 import { IState } from '../types/redux';
-import { checkUserData, notCheckUserData } from '../common/utils';
+import { checkUserData, notCheckUserData, formatDate } from '../common/utils';
 import { useUser } from '../common/hooks';
+import styled from 'styled-components'
 
 export interface IFilmGroupProps extends RouteProps {
   title: string,
   collapse?: boolean
 }
+
+interface LayoutProps {
+  films: IFilm[],
+  user?: IUser|null
+}
+
+const Th = styled.th`
+  width: 10%;
+`
+
+function Tabular ({ films }: LayoutProps) {
+  return <Table dark>
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Rating</th>
+        <th>Review</th>
+        <Th>Watched</Th>
+      </tr>
+    </thead>
+    <tbody>
+      {films.map((film) => <tr key={film.title}>
+        <th scope='row'>{film.title}</th>
+        <td>{film.userData.watched.rating}</td>
+        <td>{film.userData.watched.notes}</td>
+        <td>{formatDate(new Date(film.userData.watched.dateTime))}</td>
+      </tr>)}
+    </tbody>
+  </Table>
+}
+
+function Grid ({ films, user }: LayoutProps) {
+  return <> {films.map((film) =>
+    <Col className='mb-4' lg={2} md={3} sm={4} xs={6} key={film._id as string}>
+      <LinkCustom to={`/films/${film._id}`}>
+        <CardCustom>
+          <CardImg top src={film.poster} />
+          <CardBody className='d-flex flex-column'>
+            <CardTitle className='font-weight-bold' tag='h5'>{film.title}</CardTitle>
+            <CardSubtitle className='mb-2' tag='small'>
+              {new Date(film.releaseDate as string).toDateString()}
+            </CardSubtitle>
+            {user && <BadgeWrapper film={film} />}
+          </CardBody>
+        </CardCustom>
+      </LinkCustom>
+    </Col>)}
+  </>
+}
+
 
 export default function FilmGroup ({ title, collapse }: IFilmGroupProps) {
   const [collapseState, setCollapse] = useState(collapse)
@@ -32,29 +83,16 @@ export default function FilmGroup ({ title, collapse }: IFilmGroupProps) {
     }
   })
 
-  return <>
+  return <> 
     {title && <Title>
       <h2 onClick={() => { setCollapse(!collapseState) }}>{title}</h2>
       <span className={`oi oi-caret-${collapseState ? 'bottom' : 'top'}`} />
     </Title>}
     <Collapse isOpen={!collapseState}>
       <Row>
-        {films.map((film) =>
-          <Col className='mb-4' lg={2} md={3} sm={4} xs={6} key={film._id as string}>
-            <LinkCustom to={`/films/${film._id}`}>
-              <CardCustom>
-                <CardImg top src={film.poster} />
-                <CardBody className='d-flex flex-column'>
-                  <CardTitle className='font-weight-bold' tag='h5'>{film.title}</CardTitle>
-                  <CardSubtitle className='mb-2' tag='small'>
-                    {new Date(film.releaseDate as string).toDateString()}
-                  </CardSubtitle>
-                  {user && <BadgeWrapper film={film} />}
-                </CardBody>
-              </CardCustom>
-            </LinkCustom>
-          </Col>
-        )}
+        {title === 'Watched' 
+          ? <Tabular films={films} />
+          : <Grid user={user} films={films} />}
       </Row>
     </Collapse>
   </>
